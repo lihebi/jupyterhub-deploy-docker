3899a8a35c8e0a0057a318e6910d02bec9f33575
Hebi Li <hebi@lihebi.com>
2020-09-18 23:34:00 +0800

    add nixlab-base

--- mylab/Dockerfile.nix
+++ mylab/Dockerfile.nix
@@ -1,12 +1,61 @@
-FROM nixlab
+# FROM nixlab
+FROM nvidia/cuda:11.0-runtime-ubuntu20.04
+LABEL maintainer="Hebi Li <hebi@lihebi.com>"
+# This image is intend to be used as the base image for nixlab, I name it
+# nixlab-base
 
-SHELL ["/bin/bash", "-o", "pipefail", "-c"]
-
+# RUN chmod 1777 /tmp && chmod 1777 /var/tmp
 USER root
 
 ENV NB_USER="jovyan"
 ENV NB_UID="1000"
 ENV NB_GID="100"
+
+ENV CONDA_DIR=/opt/conda \
+    SHELL=/bin/bash \
+    LC_ALL=en_US.UTF-8 \
+    LANG=en_US.UTF-8 \
+    LANGUAGE=en_US.UTF-8
+ENV PATH=$CONDA_DIR/bin:$PATH \
+    HOME=/home/$NB_USER
+ARG PYTHON_VERSION=default
+
+ENV MINICONDA_VERSION=4.8.3 \
+    MINICONDA_MD5=d63adf39f2c220950a063e0529d4ff74 \
+        CONDA_VERSION=4.8.3
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update \
+ && apt-get install -yq --no-install-recommends \
+    wget \
+    bzip2 \
+    ca-certificates \
+    sudo \
+    locales \
+    fonts-liberation \
+    run-one \
+ && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+RUN echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && \
+        locale-gen
+
+WORKDIR /tmp
+RUN wget --quiet https://repo.continuum.io/miniconda/Miniconda3-py38_${MINICONDA_VERSION}-Linux-x86_64.sh && \
+    echo "${MINICONDA_MD5} *Miniconda3-py38_${MINICONDA_VERSION}-Linux-x86_64.sh" | md5sum -c - && \
+    /bin/bash Miniconda3-py38_${MINICONDA_VERSION}-Linux-x86_64.sh -f -b -p $CONDA_DIR && \
+    rm Miniconda3-py38_${MINICONDA_VERSION}-Linux-x86_64.sh && \
+    echo "conda ${CONDA_VERSION}" >> $CONDA_DIR/conda-meta/pinned && \
+    conda config --system --prepend channels conda-forge && \
+    conda config --system --set auto_update_conda false && \
+    conda config --system --set show_channel_urls true && \
+    conda config --system --set channel_priority strict && \
+    if [ ! $PYTHON_VERSION = 'default' ]; then conda install --yes python=$PYTHON_VERSION; fi && \
+    conda list python | grep '^python ' | tr -s ' ' | cut -d '.' -f 1,2 | sed 's/$/.*/' >> $CONDA_DIR/conda-meta/pinned && \
+    conda install --quiet --yes conda && \
+    conda install --quiet --yes pip && \
+    conda update --all --quiet --yes && \
+    conda clean --all -f -y && \
+    rm -rf /home/$NB_USER/.cache/yarn
 
 # create user
 RUN echo "auth requisite pam_deny.so" >> /etc/pam.d/su && \
@@ -15,24 +64,21 @@ RUN echo "auth requisite pam_deny.so" >> /etc/pam.d/su && \
     useradd -m -s /bin/bash -G sudo -N -u $NB_UID $NB_USER && \
     chmod g+w /etc/passwd
 
+# Install jupyterhub by conda
+RUN conda install --quiet --yes \
+    'notebook=6.1.3' \
+    'jupyterhub=1.1.0' \
+    'jupyterlab=2.2.5' && \
+    conda clean --all -f -y && \
+    npm cache clean --force && \
+    jupyter notebook --generate-config
+
+
+USER root
 WORKDIR /root
-# Copy local files as late as possible to avoid cache busting
-COPY start.sh start-notebook.sh start-singleuser.sh /usr/local/bin/
-COPY jupyter_notebook_config.py /etc/jupyter/
-
-
 EXPOSE 8888
-
+COPY start-singleuser.sh /usr/local/bin/
+COPY jupyter_notebook_config.py /etc/jupyter/
 # ENTRYPOINT ["tini", "-g", "--"]
-# CMD [ "/bin/jupyterhub-singleuser", "--ip=0.0.0.0", "--no-browser", "--allow-root", "--user=$JPY_USER", "--cookie-name=$JPY_COOKIE_NAME", "--base-url=$JPY_BASE_URL", "--hub-prefix=$JPY_HUB_PREFIX", "--hub-api-url=$JPY_HUB_API_URL"]
-# CMD [ "/bin/jupyterhub-singleuser", "--ip=0.0.0.0", "--allow-root"]
-  # CMD [ "/bin/jupyter-lab", "--ip=0.0.0.0", "--no-browser", "--allow-root"]
-
-# FIXME the problem might be jupyterlab version problem, so try install
-# jupyterlab and jupyterhub from conda
-
-# CMD ["/opt/conda/bin/jupyterhub-singleuser", "--NotebookApp.default_url=/lab", "--SingleUserNotebookApp.default_url=/lab", "--allow-root", "--ip=0.0.0.0", "--port=8888", "--notebook-dir=/root", "--debug"]
-
-
-CMD ["/usr/local/bin/start-notebook.sh", "--allow-root"]
+CMD ["start-singleuser.sh", "--allow-root"]
 # CMD [ "/usr/local/bin/start-singleuser.sh", "--allow-root"]
